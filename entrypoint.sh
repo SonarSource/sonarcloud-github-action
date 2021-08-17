eb4573ea920c7a0f4508f5ce5b2325589742d537
Nuno Pereira <npereira@expediagroup.com>
2021-08-17 16:11:16 +0200

    Removes `SONAR_TOKEN` requirement
    Since SonarQube has the option to allow scan to run without authentication token. This changes removes the hard requirement from the token from the action.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [[ -z "${SONAR_TOKEN}" ]]; then
-  echo "Set the SONAR_TOKEN env variable."
-  exit 1
-fi
-
 if [[ -f "pom.xml" ]]; then
   echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
   exit 1
