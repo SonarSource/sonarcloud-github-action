5f0b5356929ddab0e0366a1c604f3f55dd0f2fe3
Cody Simms <cody.simms@sonarsource.com>
2023-12-12 13:36:18 -0600

    Check for existance of .scannerwork in cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ ! -d "${INPUT_PROJECTBASEDIR}/.scannerwork" ]; then
+    echo ".scannerwork directory not found; nothing to clean up."
+    exit
+fi
+
 _tmp_file=$(ls "${INPUT_PROJECTBASEDIR}/" | head -1)
 PERM=$(stat -c "%u:%g" "${INPUT_PROJECTBASEDIR}/$_tmp_file")
 
