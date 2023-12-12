9ddc78a17af02da5b758683c3ecdd04d2e9fa9eb
Cody Simms <cody.simms@sonarsource.com>
2023-12-12 09:58:29 -0600

    chown only .scannerwork

--- cleanup.sh
+++ cleanup.sh
@@ -5,5 +5,5 @@ set -e
 _tmp_file=$(ls "${INPUT_PROJECTBASEDIR}/" | head -1)
 PERM=$(stat -c "%u:%g" "${INPUT_PROJECTBASEDIR}/$_tmp_file")
 
-chown -R $PERM "${INPUT_PROJECTBASEDIR}/"
+chown -R $PERM "${INPUT_PROJECTBASEDIR}/.scannerwork/"
 
