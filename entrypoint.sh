d08d592c0b694607865c089cba90bbf87c9a6d89
Istvan Bodo <127833893+istvan-bodo-sonarsource@users.noreply.github.com>
2024-05-23 10:49:51 +0200

    SCSCANGHA-16 Support passing args with spaces (#79)

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,4 +27,6 @@ if [[ "$RUNNER_DEBUG" == '1' ]]; then
 fi
 
 unset JAVA_HOME
-sonar-scanner $debug_flag -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} -Dsonar.host.url=${SONARCLOUD_URL} ${INPUT_ARGS}
+
+eval "args=(${INPUT_ARGS})"
+sonar-scanner $debug_flag "-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR}" "-Dsonar.host.url=${SONARCLOUD_URL}" "${args[@]}"
